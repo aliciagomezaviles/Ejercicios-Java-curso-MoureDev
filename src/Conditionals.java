e7d30d5519718355c29e92898fd230c9f42ad0ee
aliciagomezaviles <gomez.cia@hotmail.com>
2026-08-12 10:27:34 +0200

    Condicionales

---
+++ src/Conditionals.java
@@ -0,0 +1,35 @@
+public class Conditionals {
+
+    public static void main(String[] args) {
+
+        // Condicionales
+
+        var age = 16;
+
+        if (age > 18) {
+            System.out.println("El usuario es mayor de edad");
+        } else if (age == 18) {
+            System.out.println("El usuario acaba de cumplir 18 años");
+        } else {
+            System.out.println("El usuario es menor de edad");
+        }
+
+        // switch
+
+        var day = 5;
+
+        switch (day) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            default:
+                System.out.println("No es lunes, martes o miércoles");
+        }
+    }
+}
